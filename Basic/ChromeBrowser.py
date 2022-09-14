6ded7fe54d3d0935417c768695f496202cb6ab5f
MdPorag <mdporagbsmrstu@gmail.com>
2022-09-15 00:53:05 +0600

    few changes

---
+++ Basic/ChromeBrowser.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.service import Service
+
+
+driver = webdriver.Chrome()
+#driver = webdriver.Chrome(r'C:\chrome\chromedriver.exe')
+
+url = "http://www.facebook.com/"
+
+driver.get(url)
+driver.maximize_window()
+
+time.sleep(5)
+driver.quit()
