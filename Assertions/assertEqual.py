5e697847b8ae04428c12208493d6cac0ca793f31
farihasazid8 <fariha.sazid@klovercloud.com>
2022-11-15 10:06:11 +0600

    update

--- Assertions/assertEqual.py
+++ Assertions/assertEqual.py
@@ -12,21 +12,31 @@ from selenium.webdriver.common.alert import Alert
 
 
 class Test(unittest.TestCase):
+    # def testTitle1(self):
+    #     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    #     driver.get("https://google.com/")
+    #
+    #     titleOfWebPage = driver.title
+    #
+    #     self.assertEqual("Google", titleOfWebPage, "Webpage Title is not same ")  # True
+    #
+    # def testTitle2(self):
+    #     driver = webdriver.Chrome()
+    #     driver.get("https://google.com/")
+    #
+    #     titleOfWebPage = driver.title
+    #
+    #     self.assertEqual("Google123", titleOfWebPage, "Webpage Title is not same ")  # False
+
     def testTitle1(self):
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get("https://google.com/")
+        driver.get("https://www.facebook.com/")
 
-        titleOfWebPage = driver.title
+        lap = WebDriverWait(driver, 30).until(
+                EC.invisibility_of_element_located((By.XPATH, "//button[@id='u_0_5_cs']"))).text
+        print(lap)
 
-        self.assertEqual("Google", titleOfWebPage, "Webpage Title is not same ")  # True
-
-    def testTitle2(self):
-        driver = webdriver.Chrome()
-        driver.get("https://google.com/")
-
-        titleOfWebPage = driver.title
-
-        self.assertEqual("Google123", titleOfWebPage, "Webpage Title is not same ")  # False
+        self.assertEqual("Google", lap, "Webpage Title is not same ")  # True
 
 
 if __name__ == "__main__":
--- refresh/refresh.py
+++ refresh/refresh.py
@@ -1,12 +1,11 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
 st1 = time.time()
-driver.get("http://app1.helwan.edu.eg/Commerce/HasasnUpMlist.asp")
+driver.get("https://www.google.com/")
 driver.implicitly_wait(20)
 time.sleep(2)
-driver.refresh()
+# driver.refresh()
