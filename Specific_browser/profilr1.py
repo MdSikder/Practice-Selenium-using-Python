4484e0c761d7e0a2744cf4de09152e35b62087ac
MdSikder <mdporagbsmrstu@gmail.com>
2023-08-04 21:41:56 +0600

    few updates

---
+++ Specific_browser/profilr1.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver as uc
+
+options = Options()
+options.add_argument('--profile-directory=Profile 1')
+
+# options.add_argument("user-data-dir=C:\\Users\\KloverCloud\\AppData\\Local\\Google\\Chrome\\User Data")
+
+options.add_argument("--user-data-dir=C:\\Users\\KloverCloud\\AppData\\Local\\Google\\Chrome\\User Data")
+browser_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+driver = uc.Chrome(executable_path=browser_path, options=options)
+
+driver.maximize_window()
+driver.get("https://www.facebook.com/")
+time.sleep(5)
+driver.close()
