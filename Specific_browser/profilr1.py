74d0c7eeb772b0da30145e1a417df006b3918013
MdSikder <mdporagbsmrstu@gmail.com>
2023-08-04 23:55:40 +0600

    few updates

--- Specific_browser/profilr1.py
+++ Specific_browser/profilr1.py
@@ -8,11 +8,13 @@ options.add_argument('--profile-directory=Profile 1')
 
 # options.add_argument("user-data-dir=C:\\Users\\KloverCloud\\AppData\\Local\\Google\\Chrome\\User Data")
 
-options.add_argument("--user-data-dir=C:\\Users\\KloverCloud\\AppData\\Local\\Google\\Chrome\\User Data")
-browser_path = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+
+options.add_argument("--user-data-dir=C:/Users/KloverCloud/AppData/Local/Google/Chrome/User Data")
+browser_path = "C:/Program Files/Google/Chrome/Application/chrome.exe"
 driver = uc.Chrome(executable_path=browser_path, options=options)
 
 driver.maximize_window()
-driver.get("https://www.facebook.com/")
+driver.get('https://www.facebook.com/')
+driver.get("https://mail.google.com/mail/u/0/#inbox")
 time.sleep(5)
 driver.close()
